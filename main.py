671135f3d0b1f5818bd11579c5429cf4bc734305
Lakshay24rohilla <156428348+Lakshay24rohilla@users.noreply.github.com>
2024-02-01 17:32:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ bot = Client(
     "bot",
     api_id=23020129,
     api_hash="1e7e17574020886a8b43ebc39d58e616",
-    bot_token="6787528822:AAEVOewwSAXIOmDD7seKCKVTvVmENDsQhTc")
+    bot_token="6888105317:AAH0FoMj3myzAMNhtpr-GiXjoeen8C50gRE")
       
 @bot.on_message(filters.command(["start"])&(filters.chat(auth_users)))
 async def start_handler(bot: Client, m: Message):        
