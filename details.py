30e2245fad50dca6b49f15d362e7b708f6dff9a2
Lakshay24rohilla <156428348+Lakshay24rohilla@users.noreply.github.com>
2024-02-01 17:18:00 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 23020129
 api_hash = "1e7e17574020886a8b43ebc39d58e616"
-bot_token = "6787528822:AAEVOewwSAXIOmDD7seKCKVTvVmENDsQhTc"
-auth_users = [6811710742,5374602611]
-sudo_user = -1002072991536
+bot_token = "6888105317:AAH0FoMj3myzAMNhtpr-GiXjoeen8C50gRE"
+auth_users = 6811710742
+sudo_user = 5374602611
 log_channel = -1001967910749
 txt_channel = -1002046876733
