05bbf20bd71913d820d5f76645caa43b463fd51e
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-02-22 11:50:51 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 23020129
-api_hash = "1e7e17574020886a8b43ebc39d58e616"
-bot_token = "6888105317:AAH0FoMj3myzAMNhtpr-GiXjoeen8C50gRE"
-auth_users = [6811710742,5374602611,1400464289]
-sudo_user = -4172620031
-log_channel = -1001967910749
-txt_channel = -1002046876733
+api_id = 20544260
+api_hash = "a0b00461d3fba22aa186fa648d77787e"
+bot_token = "6968574821:AAH0ZPX1aRJfyE0hjn8iJermZcU8hZiGDJI"
+auth_users = [6389388334]
+sudo_user = -1002117596251
+log_channel = -1002117596251
+txt_channel = -1002117596251
